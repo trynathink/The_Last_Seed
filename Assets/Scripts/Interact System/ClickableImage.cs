a19706a4d0e901b26207aca1565a8f4f930c4503
Insane <insaneshowman@gmail.com>
2026-07-12 15:16:17 -0400

    local save

--- Assets/Scripts/Interact System/ClickableImage.cs
+++ Assets/Scripts/Interact System/ClickableImage.cs
@@ -6,13 +6,6 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class ClickableImage : MonoBehaviour, IPointerClickHandler
 {
-
-    // Vinayak Karuppasamy
-
-    // This script can be attached to sprites we want to add click behaviour to
-    // and `OnPointerClick` will get triggered if a click event is registered on
-    // the non transparent pixels of that image
-
     [Range(0f, 1f)]
     [SerializeField]
     private float alphaThreshold = 0.1f;
---
+++ Assets/Scripts/Living Room Button Manager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LivingRoomButtonManager : MonoBehaviour
+{
+    // Gaurav Singh
+
+    // Contains the buttons methods for the Living Room
+    // Acts as the Manager for the Living Room Screen
+
+    public PlayerDataSO PDSO;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
