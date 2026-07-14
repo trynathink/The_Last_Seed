9eab6e14cb01a63df28c8f0a937362157f52a36a
alego <alexgottuso@gmail.com>
2026-07-13 22:16:23 -0700

    add button sfx for main menu

---
+++ Assets/Scripts/Audio/ButtonSFX.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSFX : MonoBehaviour
+{
+	[SerializeField] private AudioClip clickSound;
+	[SerializeField] private AudioSource source;
+	[SerializeField] private GameObject canvas;
+
+	private void OnEnable()
+	{
+		foreach (Button button in canvas.GetComponentsInChildren<Button>())
+		{
+			button.onClick.AddListener(PlaySound);
+		}
+	}
+
+	private void PlaySound()
+	{
+		source.PlayOneShot(clickSound);
+	}
+}
