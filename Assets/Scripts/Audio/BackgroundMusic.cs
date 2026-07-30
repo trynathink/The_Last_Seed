2b9557782e04de808a236b8b843a196d96b1a1cf
alego <alexgottuso@gmail.com>
2026-07-29 17:01:01 -0700

    add fade in/out for bgm

--- Assets/Scripts/Audio/BackgroundMusic.cs
+++ Assets/Scripts/Audio/BackgroundMusic.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
+using System.Collections;
 
 // Alexander Gottuso
 
@@ -9,16 +9,43 @@ using System.Collections.Generic;
 
 public class BackgroundMusic : AudioSingleton<BackgroundMusic>
 {
+	const float fadeTime = 1f; // Time in seconds for fade in and out respectively
+
 	// NOTE: This override assumes that the clip on the AudioSource for this BGM contains that scene's song
 	protected override void Awake()
 	{
-		AudioClip next = gameObject.GetComponent<AudioSource>().clip;
+		AudioSource current = gameObject.GetComponent<AudioSource>();
 
-		if (source != null && next != source.clip)
+		if (source != null && current.clip != source.clip)
 		{
-			source.clip = next;
-			source.Play();
+			current.Stop();
+			StartCoroutine(Fade(current.clip));
 		}
+		else
+		{
+			base.Awake();
+		}
+	}
+
+	private IEnumerator Fade(AudioClip next)
+	{
+		float startVolume = source.volume;
+
+        while (source.volume > 0)
+		{
+            source.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+
+        source.Stop();
+		source.clip = next;
+		source.Play();
+
+        while (source.volume < 1)
+		{
+            source.volume += Time.deltaTime / fadeTime;
+            yield return null;
+        }
 
 		base.Awake();
 	}
